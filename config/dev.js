f1d56e578e1fb99d568738eb098272c9933e77a3
boo <srmsqn@naver.com>
2023-02-19 14:41:30 +0900

    비밀 정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://geunboo:asdf664417@boo-zone.zqgg2bw.mongodb.net/?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,12 +1,35 @@
 const express = require('express')
 const app = express()
 const port = 7000
+const bodyParser = require('body-parser')
+const { User } = require("./model/User")
+
+const config = require('./config/key')
+
+//application/x-www-form-urlencoded 타입을 분석해서 가져옴
+app.use(bodyParser.urlencoded({extended: true}))
+//json 타입 분석해서 가져옴
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
 mongoose.set("strictQuery", true);
-mongoose.connect('mongodb+srv://geunboo:asdf664417@boo-zone.zqgg2bw.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true
 }).then(() => console.log('몽고 연결!'))
     .catch(err => console.log(err))
+
 app.get('/', (req, res) => res.send('Hello'))
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
