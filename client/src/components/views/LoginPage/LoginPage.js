636a4422e379fdc5a0264f58f4562376d63826f3
boo <srmsqn@naver.com>
2023-02-25 01:01:38 +0900

    login in progress

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Axios from 'axios'
+import {useDispatch} from 'react-redux'
 
 function LoginPage() {
+
+    const [Id, setId] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onIdHandler = (event) => {
+        setId(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault()
+
+        let body = {
+            id : Id,
+            password : Password
+        }
+
+        Axios.post('/api/users/login', body)
+            .then(response => {
+
+            })
+    }
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: '100%', height: '100vh'
+        }}>
+            <form style={{display:'flex', flexDirection: 'column'}}
+                onSubmit={onSubmitHandler}
+            >
+                <label>ID</label>
+                <input type="text" value={Id} onChange={onIdHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br/>
+                <button>
+                    Login
+                </button>
+            </form>
         </div>
     )
 }
